708114c562e0560ac78bca8611fdeb7ae493e4a5
nipan09 <pnishant46@gmail.com>
2020-04-18 17:31:18 +0530

    migrated file

--- project/shop/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from shop.models import products 
-
-def home_view(request):
-	objects = products.objects.all()
-	return render(request, 'home.html', {'products': objects})
