ca573748ba03208f405a3a83d3c57892e87c183d
nipan09 <pnishant46@gmail.com>
2020-04-21 23:45:48 +0530

    feat: add to cart added

--- project/shop/urls.py
+++ project/shop/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-     path('shop', views.home_view, name='home')
+     path('shop', views.home_view, name='home'),
+     path('shop/cart/<slug>', views.add_cart_view, name='addCart'),
 ]
--- project/shop/views.py
+++ project/shop/views.py
@@ -1,6 +1,13 @@
-from shop.models import products
-from django.shortcuts import render, redirect
+from shop.models import products, cart
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
 
 def home_view(request):
 	context = products.objects.all()
 	return render(request, 'home.html', {'products':context})
+
+def add_cart_view(request, slug):
+	item = get_object_or_404(products, slug=slug)
+	cart.objects.create(user=request.user, item=item)
+	messages.info(request, "This item has been added to your cart")
+	return redirect('home')
