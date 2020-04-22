d2b362682e217b8788abc310b0d8dc728933c2e8
nipan09 <pnishant46@gmail.com>
2020-04-22 10:28:19 +0530

    order model added

--- project/shop/admin.py
+++ project/shop/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from shop.models import products, cart
+from shop.models import products, cart, order
 
 admin.site.register(products)
 admin.site.register(cart)
+admin.site.register(order)
