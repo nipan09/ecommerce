0b4bca9fba60eb26d3648db39ef5941520b4e7b4
nipan09 <pnishant46@gmail.com>
2020-05-02 12:12:39 +0530

    cart url added

--- project/shop/urls.py
+++ project/shop/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-     path('shop', views.home_view, name='home'),
+     path('shop/', views.home_view, name='home'),
      path('shop/cart/<slug>', views.add_cart_view, name='addCart'),
      path('shop/removeCart/<slug>', views.remove_cart_view, name='removeCart'),
+     path('shop/cart', views.cart_view, name='cart'),
 ]
