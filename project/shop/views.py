85f18e3747f30747c6c77a3ade6911fb5692d08c
nipan09 <pnishant46@gmail.com>
2020-05-02 19:29:41 +0530

    order model deleted

--- project/shop/views.py
+++ project/shop/views.py
@@ -1,4 +1,4 @@
-from shop.models import products, cart, order
+from shop.models import products, cart
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 
