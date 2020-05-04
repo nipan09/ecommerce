1c44de69b3b2569c22eebeb895a194eca09a1ffa
nipan09 <pnishant46@gmail.com>
2020-05-04 23:22:33 +0530

    order url created

--- project/shop/urls.py
+++ project/shop/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns=[
      path('shop/', views.home_view, name='home'),
-     path('shop/cart/<slug>', views.add_cart_view, name='addCart'),
+     path('shop/addcart/<slug>', views.add_cart_view, name='addCart'),
      path('shop/removeCart/<slug>', views.remove_cart_view, name='removeCart'),
      path('shop/cart', views.cart_view, name='cart'),
+     path('shop/cart/order/<slug>', views.order_view, name='order'),
 ]
