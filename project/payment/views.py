3ffa16c111db990624fb0c6569e251d90c82e952
nipan09 <pnishant46@gmail.com>
2020-05-06 23:45:17 +0530

    payment updated

--- project/payment/views.py
+++ project/payment/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.conf import settings
+from shop.models import cart
 import stripe
 
 stripe.api_key = settings.STRIPE_SECRET_KEY
 
 def pay_view(request, slug):
-	return render(request,'payment.html', {'slug':slug})
+	cart_qs = cart.objects.filter(user=request.user, slug=slug)
+	cart_obj = cart_qs[0]
+	amount = cart_obj.get_total()
+	return render(request,'payment.html', {'slug':slug, 'amount':amount})
